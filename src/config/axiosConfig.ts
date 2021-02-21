5237460c85d0810333b3699dc6e9a12aea46ce97
Vladimir Lipovski <v.lypivskyy@gmail.com>
2021-02-21 18:08:36 +0100

    Fix cors

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -3,8 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com',
   headers: {
-    'Access-Control-Allow-Origin': '*',
-    Accept: 'application/json;odata.metadata=full',
     'Content-Type': 'application/json',
   },
 });
