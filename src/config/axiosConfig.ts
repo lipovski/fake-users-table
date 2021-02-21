cf74ac0c86480dbbddd98878f84173fdd81b12cd
Vladimir Lipovski <v.lypivskyy@gmail.com>
2021-02-21 17:49:49 +0100

    change axios config

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -2,4 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com',
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    'Access-Control-Allow-Origin': '*',
+  },
 });
